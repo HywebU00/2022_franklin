4d94e299c5dd9860738a7dc3790685a10e24dea3
Your Namemargo-lee <margo.lee@hyweb.com.tw>
2022-05-11 11:18:54 +0800

    更新頁面

---
+++ js/popover_setting.js
@@ -0,0 +1,21 @@
+var popover = new bootstrap.Popover(document.querySelector(".popover-buy"), {
+  trigger: "focus",
+});
+var popover2 = new bootstrap.Popover(document.querySelector(".popover-debit"), {
+  trigger: "focus",
+});
+var popover3 = new bootstrap.Popover(document.querySelector(".popover-add"), {
+  trigger: "focus",
+});
+var popover4 = new bootstrap.Popover(
+  document.querySelector(".popover-reduce"),
+  {
+    trigger: "focus",
+  }
+);
+var popover5 = new bootstrap.Popover(document.querySelector(".popover-month"), {
+  trigger: "focus",
+});
+var popover6 = new bootstrap.Popover(document.querySelector(".popover-fund"), {
+  trigger: "focus",
+});
