4f15fff631d21fb5b31541c2c1641d1a84e0ef46
Your Namemargo-lee <margo.lee@hyweb.com.tw>
2022-09-08 14:32:16 +0800

    新增loading效果

--- js/ftft.js
+++ js/ftft.js
@@ -1,10 +1,10 @@
-const popOver = document.querySelector(".popover-dismiss");
+const popOver = document.querySelector('.popover-dismiss');
 if (popOver !== null) {
   var popover = new bootstrap.Popover(popOver, {
-    trigger: "focus",
+    trigger: 'focus',
     template:
       '<div class="tooltip" role="tooltip"><ul><li><img src="image/icon-username.png" alt=""><span>客戶姓名 :</span>王大明</li><li><img src="image/icon-account.png" alt=""><span>指定扣款帳號 :</span>臺灣銀行00000000****</li><li><img src="image/icon-pay.png" alt=""><span>投信戶號 :</span>45251</li></ul></div>',
-    placement: "bottom",
+    placement: 'bottom',
   });
 }
 //======================
@@ -12,22 +12,22 @@ if (popOver !== null) {
 //======================
 // Example starter JavaScript for disabling form submissions if there are invalid fields
 (function () {
-  "use strict";
+  'use strict';
 
   // Fetch all the forms we want to apply custom Bootstrap validation styles to
-  var forms = document.querySelectorAll(".needs-validation");
+  var forms = document.querySelectorAll('.needs-validation');
 
   // Loop over them and prevent submission
   Array.prototype.slice.call(forms).forEach(function (form) {
     form.addEventListener(
-      "submit",
+      'submit',
       function (event) {
         if (!form.checkValidity()) {
           event.preventDefault();
           event.stopPropagation();
         }
 
-        form.classList.add("was-validated");
+        form.classList.add('was-validated');
       },
       false
     );
@@ -36,17 +36,17 @@ if (popOver !== null) {
 //======================
 // 點擊 menu表單 更改樣式
 //======================
-const navItem = document.querySelectorAll(".nav-toggle");
+const navItem = document.querySelectorAll('.nav-toggle');
 function removeItemClass() {
   navItem.forEach((item) => {
-    item.classList.remove("show");
+    item.classList.remove('show');
   });
 }
 if (navItem != null) {
   navItem.forEach((item) => {
-    item.addEventListener("click", function () {
+    item.addEventListener('click', function () {
       removeItemClass();
-      this.classList.add("show");
+      this.classList.add('show');
     });
   });
 }
@@ -54,27 +54,27 @@ if (navItem != null) {
 //======================
 // 點擊 searchMobile 展開
 //======================
-const searchMobile = document.querySelector(".search-mobile");
-const userMobilePopover = document.querySelector(".user-mobile-popover");
+const searchMobile = document.querySelector('.search-mobile');
+const userMobilePopover = document.querySelector('.user-mobile-popover');
 if (searchMobile != null) {
-  searchMobile.addEventListener("click", function () {
-    userMobilePopover.classList.toggle("show");
+  searchMobile.addEventListener('click', function () {
+    userMobilePopover.classList.toggle('show');
   });
 }
 
 //========================
 // 點擊 密碼icon 改變密碼狀態
 //========================
-const EyeBtn = document.querySelectorAll(".btn-eye");
-if (EyeBtn != null) {
-  EyeBtn.forEach((item) => {
-    item.addEventListener("click", () => {
-      if (item.previousElementSibling.type === "password") {
-        item.previousElementSibling.type = "text";
+const eyeBtn = document.querySelectorAll('.btn-eye');
+if (eyeBtn != null) {
+  eyeBtn.forEach((item) => {
+    item.addEventListener('click', () => {
+      if (item.previousElementSibling.type === 'password') {
+        item.previousElementSibling.type = 'text';
       } else {
-        item.previousElementSibling.type = "password";
+        item.previousElementSibling.type = 'password';
       }
-      item.classList.toggle("bi-eye");
+      item.classList.toggle('bi-eye');
     });
   });
 }
@@ -82,13 +82,13 @@ if (EyeBtn != null) {
 //==========================
 // 停利報酬 modal的input 判斷
 //==========================
-const PayInput = document.querySelectorAll(".form-control-pay");
-PayInput.forEach(function (item) {
-  item.addEventListener("change", function (e) {
+const payInput = document.querySelectorAll('.form-control-pay');
+payInput.forEach(function (item) {
+  item.addEventListener('change', function (e) {
     let value = e.target.value;
     addDisabled();
     if (value.length > 0) {
-      item.removeAttribute("disabled");
+      item.removeAttribute('disabled');
     } else {
       removeDisabled();
     }
@@ -96,13 +96,35 @@ PayInput.forEach(function (item) {
   //新增所有 disabled 標籤
   function addDisabled() {
     PayInput.forEach(function (item) {
-      item.setAttribute("disabled", "");
+      item.setAttribute('disabled', '');
     });
   }
   //移除所有 disabled 標籤
   function removeDisabled() {
     PayInput.forEach(function (item) {
-      item.removeAttribute("disabled");
+      item.removeAttribute('disabled');
     });
   }
 });
+//==========================
+// loading 效果
+//==========================
+const loading = document.querySelector('.loading');
+function addRemoveClass() {
+  loading.classList.add('remove');
+}
+
+lottie.loadAnimation({
+  container: document.querySelector('.loading-img'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'image/loading-img.json',
+});
+
+//模擬 loading 出現的效果
+if (loading !== null) {
+  setTimeout(() => {
+    addRemoveClass();
+  }, '5000');
+}
--- js/mp.js
+++ js/mp.js
@@ -1,14 +1,14 @@
 var options = {
   series: [44, 55, 13, 33],
   chart: {
-    type: "donut",
+    type: 'donut',
   },
 
-  colors: ["#013e8e", "#0086b7", "#00b3c0", "#ff7353"],
+  colors: ['#013e8e', '#0086b7', '#00b3c0', '#ff7353'],
   dataLabels: {
     enabled: false,
   },
-  labels: ["股票型", "債券型", "平衡型", "貨幣型"],
+  labels: ['股票型', '債券型', '平衡型', '貨幣型'],
 
   responsive: [
     {
@@ -77,21 +77,21 @@ var options = {
     },
   ],
   legend: {
-    position: "right",
+    position: 'right',
     offsetY: 0,
     height: 230,
     show: false,
   },
   title: {
-    text: "基金類型",
-    align: "center",
+    text: '基金類型',
+    align: 'center',
     offsetY: 150,
     floating: true,
     style: {
-      fontSize: "16px",
-      fontWeight: "bold",
-      fontFamily: undefined,
-      color: "#263238",
+      fontSize: '16px',
+      fontWeight: 'bold',
+      fontFamily: 'Noto Sans TC',
+      color: '#263238',
     },
   },
 };
@@ -99,14 +99,14 @@ var options = {
 var options2 = {
   series: [44, 55, 13, 33],
   chart: {
-    type: "donut",
+    type: 'donut',
   },
 
-  colors: ["#013e8e", "#0086b7", "#00b3c0", "#ff7353"],
+  colors: ['#013e8e', '#0086b7', '#00b3c0', '#ff7353'],
   dataLabels: {
     enabled: false,
   },
-  labels: ["新台幣", "美元", "日圓", "人民幣"],
+  labels: ['新台幣', '美元', '日圓', '人民幣'],
 
   responsive: [
     {
@@ -175,21 +175,21 @@ var options2 = {
     },
   ],
   legend: {
-    position: "right",
+    position: 'right',
     offsetY: 0,
     height: 230,
     show: false,
   },
   title: {
-    text: "幣別𢑥總",
-    align: "center",
+    text: '幣別𢑥總',
+    align: 'center',
     offsetY: 150,
     floating: true,
     style: {
-      fontSize: "16px",
-      fontWeight: "bold",
-      fontFamily: undefined,
-      color: "#263238",
+      fontSize: '16px',
+      fontWeight: 'bold',
+      fontFamily: 'Noto Sans TC',
+      color: '#263238',
     },
   },
 };
@@ -197,20 +197,19 @@ var options2 = {
 var options3 = {
   series: [44, 55, 13, 13, 10, 10],
   chart: {
-    type: "donut",
+    type: 'donut',
   },
-
-  colors: ["#013e8e", "#0086b7", "#006d77", "#00b3c0", "#ff7353", "#9ad4de"],
+  colors: ['#013e8e', '#0086b7', '#006d77', '#00b3c0', '#ff7353', '#9ad4de'],
   dataLabels: {
     enabled: false,
   },
   labels: [
-    "AI新科技基金",
-    "特別股收益基金",
-    "全球高收益債券基金",
-    "新世界股票基金",
-    "第一富基金",
-    "其他",
+    'AI新科技基金',
+    '特別股收益基金',
+    '全球高收益債券基金',
+    '新世界股票基金',
+    '第一富基金',
+    '其他',
   ],
 
   responsive: [
@@ -280,28 +279,28 @@ var options3 = {
     },
   ],
   legend: {
-    position: "right",
+    position: 'right',
     offsetY: 0,
     height: 230,
     show: false,
   },
   title: {
-    text: "個別基金",
-    align: "center",
+    text: '個別基金',
+    align: 'center',
     offsetY: 150,
     floating: true,
     style: {
-      fontSize: "16px",
-      fontWeight: "bold",
-      fontFamily: undefined,
-      color: "#263238",
+      fontSize: '16px',
+      fontWeight: 'bold',
+      fontFamily: 'Noto Sans TC',
+      color: '#263238',
     },
   },
 };
 
-var chart = new ApexCharts(document.querySelector("#chart"), options);
+var chart = new ApexCharts(document.querySelector('#chart'), options);
 chart.render();
-var chart2 = new ApexCharts(document.querySelector("#chart2"), options2);
+var chart2 = new ApexCharts(document.querySelector('#chart2'), options2);
 chart2.render();
-var chart3 = new ApexCharts(document.querySelector("#chart3"), options3);
+var chart3 = new ApexCharts(document.querySelector('#chart3'), options3);
 chart3.render();
